06e7c261c028241af6acebbb7bab4c63e231aff4
muzzafer5 <muzzaferali5@gmail.com>
2021-08-01 20:38:46 +0530

    setup mysql db and sequelize orm

---
+++ config/database.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+module.exports = {
+  development: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  test: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  },
+  production: {
+    username: process.env.PROD_DB_USERNAME,
+    password: process.env.PROD_DB_PASSWORD,
+    database: process.env.PROD_DB_NAME,
+    host: process.env.PROD_DB_HOSTNAME,
+    port: process.env.PROD_DB_PORT,
+    dialect: 'mysql',
+    dialectOptions: {
+      bigNumberStrings: true
+    }
+  }
+};
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ require("dotenv").config()
 const app = require('./route')
 const port = process.env.APP_PORT
 
+var models = require("./models");
+
+models.sequelize.sync().then(() => {
+    console.log("connected")
+}).catch(() => {
+    console.log("error")
+})
+
 app.listen(port, function () {
     console.log('Server is running on port: ' + port)
 });
