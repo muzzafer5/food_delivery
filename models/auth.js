86578b1bc3aecd90df79dab9ec243ff1b648e22f
muzzafer5 <muzzaferali5@gmail.com>
2021-08-05 09:48:43 +0530

    Added auth model for user authentication

---
+++ models/auth.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Auth extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Auth.hasOne(models.User, {
+        foreignKey: 'authId'
+      })
+    }
+  };
+  Auth.init({
+    username: {
+      type : DataTypes.STRING,
+      allowNull : false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    modelName: 'Auth',
+  });
+  return Auth;
+};
