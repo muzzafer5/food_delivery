7655d98328e4e8e7c6a036c73d0782fe713c8367
muzzafer5 <muzzaferali5@gmail.com>
2021-08-11 23:29:30 +0530

    modify config for production mysql

--- config/database.js
+++ config/database.js
@@ -25,11 +25,11 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.PROD_DB_USERNAME,
-    password: process.env.PROD_DB_PASSWORD,
-    database: process.env.PROD_DB_NAME,
-    host: process.env.PROD_DB_HOSTNAME,
-    port: process.env.PROD_DB_PORT,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     dialect: 'mysql',
     dialectOptions: {
       bigNumberStrings: true
